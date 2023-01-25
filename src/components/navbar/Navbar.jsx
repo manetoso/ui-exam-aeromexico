8a14a09e7d8dd700c7403c981fdcc44f36dea1b4
“manetoso” <manetoso98@hotmail.com>
2023-01-25 16:04:22 -0600

    feat: navbar styled

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,14 +4,25 @@ import AddIcon from '../../assets/icons/add-character-fill.svg';
 export const Navbar = () => {
   return (
     <nav className='nav'>
-      <button className='nav-button'>
-        <span className='nav-button-label'>favoritos</span>
-        <img className='nav-button-icon' src={BookmarkIcon} alt='bookmark icon' />
-      </button>
-      <button>
-        <span className='nav-button-label'>agregar</span>
-        <img className='nav-button-icon' src={AddIcon} alt='add icon' />
-      </button>
+      <div className='nav-container'>
+        <div className='nav-wrapper'>
+          <div className='nav-buttons-wrapper'>
+            <button className='nav-button'>
+              <span className='nav-button-label'>favoritos</span>
+              <img
+                className='nav-button-icon'
+                src={BookmarkIcon}
+                alt='bookmark icon'
+              />
+            </button>
+            <div className='separator' />
+            <button className='nav-button'>
+              <span className='nav-button-label'>agregar</span>
+              <img className='nav-button-icon' src={AddIcon} alt='add icon' />
+            </button>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
