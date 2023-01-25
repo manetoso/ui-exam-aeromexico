4d3db7bb74f21cd9dee4b0dc9459bbc97d2f727c
“manetoso” <manetoso98@hotmail.com>
2023-01-25 09:39:15 -0600

    feat: styling the card

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,5 +1,6 @@
-// import NoBookmarked from './../../assets/icons/bookmark-outline.svg'
-import Bookmarked from './../../assets/icons/bookmark-fill-black.svg';
+import NoBookmarked from './../../assets/icons/bookmark-outline.svg'
+import { useEffect, useState } from 'react';
+// import Bookmarked from './../../assets/icons/bookmark-fill-black.svg';
 
 const placeholderData = {
   id: 8,
@@ -28,27 +29,42 @@ const placeholderData = {
 export const Card = () => {
   return (
     <div className='card-wrapper'>
-      <img className='card-image' src={placeholderData.image} alt='character' />
+      <div className='card-image-container card-gryffindor'>
+        <img className='card-image' src={placeholderData.image} alt='character' />
+      </div>
       <div className='card-body'>
-        <h4 className='card-label'>{`${
-          placeholderData.alive ? 'vivo' : 'finado'
-        } / ${placeholderData.hogwartsStudent ? 'estudiante' : 'staff'}`}</h4>
+        <div className='card-header header-lg'>
+          <h4 className='card-header-label'>{`${
+            placeholderData.alive ? 'vivo' : 'finado'
+          } / ${placeholderData.hogwartsStudent ? 'estudiante' : 'staff'}`}</h4>
+          <img className='card-header-icon' src={NoBookmarked} alt='bookmark icon' />
+        </div>
         <h3 className='card-title'>{placeholderData.name}</h3>
+        <div className='card-header header-sm'>
+          <span>
+            <h4 className='card-header-label'>
+              {placeholderData.alive ? 'vivo' : 'finado'}
+            </h4>
+            <h4 className='card-header-label'>
+              {placeholderData.hogwartsStudent ? 'estudiante' : 'staff'}
+            </h4>
+          </span>
+          <img className='card-header-icon' src={NoBookmarked} alt='bookmark icon' />
+        </div>
         <ul className='card-details'>
           <li className='card-spec'>
-            <strong>Cumplea&ntilde;os:</strong> {placeholderData.dateOfBirth}
+            <strong>Cumplea&ntilde;os:</strong> <span>{placeholderData.dateOfBirth}</span>
           </li>
           <li className='card-spec'>
-            <strong>Genero:</strong> {placeholderData.gender}
+            <strong>Genero:</strong> <span>{placeholderData.gender}</span>
           </li>
           <li className='card-spec'>
-            <strong>Color de ojos:</strong> {placeholderData.eyeColour}
+            <strong>Color de ojos:</strong> <span>{placeholderData.eyeColour}</span>
           </li>
           <li className='card-spec'>
-            <strong>Color de pelo:</strong> {placeholderData.hairColour}
+            <strong>Color de pelo:</strong> <span>{placeholderData.hairColour}</span>
           </li>
         </ul>
-        <img className='card-icon' src={Bookmarked} alt='bookmark icon' />
       </div>
     </div>
   );
